7dbd5e8d70ed796cee2c25afc18d208729f76f4e
jnotnull <252210289@qq.com>
2017-04-02 14:47:05 +0800

    优化接口调用

--- templates/proxy.js
+++ templates/proxy.js
@@ -1,8 +1,8 @@
-import {nodeserver} from '../common/config';
-import {sendrequest} from './request';
+import {sendrequest, upload} from 'perfect-fetch';
 
 export function test(){
     return sendrequest(`${nodeserver}/xx/xx/xx`, {
-
+		id: 1,
+		name: "jnotnull"
     })
 }
